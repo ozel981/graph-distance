542d48fbbc9dd24a2a9f180571033fbe3e25b137
Jakub Michalak <michalakjakub1999@gmail.com>
2021-10-31 11:27:36 +0100

    Init graph class

--- GraphDistance/GraphDistance/Program.cs
+++ GraphDistance/GraphDistance/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using GraphLibrary;
 
 namespace GraphDistance
 {
@@ -7,6 +8,8 @@ namespace GraphDistance
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Graph g = new Graph(new double[,]{{0,1},{1,0}});
+            Console.WriteLine(g);
         }
     }
 }
--- GraphDistance/GraphLibrary/Graph.cs
+++ GraphDistance/GraphLibrary/Graph.cs
@@ -1,8 +1,42 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+using MathNet.Numerics.LinearAlgebra;
 
 namespace GraphLibrary
 {
     public class Graph
     {
+        private Matrix<double> edges;
+
+        public Graph(double[,] edges)
+        {
+            if (edges.Rank != 2)
+            {
+                throw new ArgumentException($"invalid array rank: expected 2, got {edges.Rank}");
+            }
+            var rowCount = edges.GetLength(0);
+            var colCount = edges.GetLength(1);
+            if (rowCount != colCount)
+            {
+                throw new ArgumentException($"row count ({rowCount}) differs from column count ({colCount})");
+            }
+            this.edges = Matrix<double>.Build.DenseOfArray(edges);
+        }
+
+        public Graph NewCompatibilityGraph(Graph g, Graph h)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public int VerticesCount()
+        {
+            return this.edges.RowCount;
+        }
+
+        public override string ToString()
+        {
+            return $"Graph with {this.VerticesCount()} vertices.";
+        }
     }
 }
