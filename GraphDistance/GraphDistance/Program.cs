c8cc0f276ca51007a2a7954ad1b7e47469ca874d
Jakub Michalak <michalakjakub1999@gmail.com>
2021-11-15 17:16:17 +0100

    Read graphs from file (#26)

--- GraphDistance/GraphDistance/Program.cs
+++ GraphDistance/GraphDistance/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Text;
 using GraphLibrary;
 using MathNet.Numerics.LinearAlgebra;
 
@@ -11,8 +14,7 @@ namespace GraphDistance
         {
             try
             {
-                var g = new Graph(ReadMatrix());
-                var h = new Graph(ReadMatrix());
+                (var g, var h) = ReadGraphs();
                 var algorithms = ReadAlgorithms();
                 if (algorithms.Contains("a"))
                 {
@@ -78,10 +80,12 @@ namespace GraphDistance
             Console.WriteLine($"Rozmiar kliki: {vertices.Count}");
             Console.WriteLine($"Wynik aproksymacyjnego algorytmu: {result}");
         }
-        static double[,] ReadMatrix()
+        static double[,] ReadMatrixFromStdin()
         {
+            Console.WriteLine("Podaj liczbę wierzchołków grafu:");
             var n = Convert.ToInt32(Console.ReadLine());
             double[,] edges = new double[n,n];
+            Console.WriteLine("Podaj macierz sąsiedztwa grafu:");
             for (int i = 0; i < n; i++)
             {
                 var row = Console.ReadLine()?.Split();
@@ -96,6 +100,58 @@ namespace GraphDistance
             }
             return edges;
         }
+
+        static (Graph g, Graph h) ReadGraphs()
+        {
+            Console.WriteLine("Podaj ścieżkę do pliku z wejściem (jeśli grafy mają być wpisane" +
+                              "ręcznie, wciśnij ENTER):");
+            var path = Console.ReadLine();
+            if (String.IsNullOrEmpty(path))
+            {
+                var g = new Graph(ReadMatrixFromStdin());
+                var h = new Graph(ReadMatrixFromStdin());
+                return (g, h);
+            }
+            const Int32 BufferSize = 128;
+            using (var fileStream = File.OpenRead(path))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+                String line = streamReader.ReadLine();
+                var n = Convert.ToInt32(line);
+                double[,] edgesG = new double[n,n];
+                for (int i = 0; i < n; i++)
+                {
+                    var row = streamReader.ReadLine()?.Split();
+                    if (row.Length != n)
+                    {
+                        throw new Exception("incorrect number of values in a row");
+                    }
+                    for (int j = 0; j < n; j++)
+                    {
+                        edgesG[i, j] = int.Parse(row[j]);
+                    }
+                }
+                line = streamReader.ReadLine();
+                var m = Convert.ToInt32(line);
+                double[,] edgesH = new double[m,m];
+                for (int i = 0; i < m; i++)
+                {
+                    var row = streamReader.ReadLine()?.Split();
+                    if (row.Length != m)
+                    {
+                        throw new Exception("incorrect number of values in a row");
+                    }
+                    for (int j = 0; j < m; j++)
+                    {
+                        edgesH[i, j] = int.Parse(row[j]);
+                    }
+                }
+                var g = new Graph(edgesG);
+                var h = new Graph(edgesH);
+                return (g, h);
+            }
+        }
+
 
         static List<string> ReadAlgorithms()
         {
