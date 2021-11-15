0aa6ad15d672d7eacca40728afcabfae85cbf64f
Wojciech Podmokły <ozel981@interia.pl>
2021-11-15 10:04:21 +0100

    Exact graph (#15)
    * recursion-scheme
    
    * exact-algorith
    
    * fix-main
    
    * Update GraphDistance/GraphLibrary/ExactAlgorithm.cs
    
    Co-authored-by: Damian Opoka <damian.opoka99@gmail.com>
    
    * Update GraphDistance/GraphLibrary/ExactAlgorithm.cs
    
    Co-authored-by: Damian Opoka <damian.opoka99@gmail.com>
    
    * solve comments
    
    Co-authored-by: Damian Opoka <damian.opoka99@gmail.com>

---
+++ GraphDistance/GraphLibrary/ExactAlgorithm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphLibrary
+{
+    public class ExactAlgorithm
+    {
+        private List<int> maxClique = new List<int>();
+        private void PrepareSets(int verticesCount, out List<int> clique, out List<int> rejected, out List<int> candidates)
+        {
+            clique = new List<int>();
+            rejected = new List<int>();
+            candidates = new List<int>();
+            for (int i = 0; i < verticesCount; i++)
+            {
+                candidates.Add(i);
+            }
+        }
+
+        private void FindMaximumCliqueRec(ref Graph graph, ref List<int> clique, ref List<int> rejected, ref List<int> candidates)
+        {
+            while (candidates.Any())
+            {
+                List<int> localRejected = new List<int>(rejected);
+                List<int> localCandidates = new List<int>(candidates);
+                int vertex = candidates.First();
+                clique.Add(vertex);
+                candidates.Remove(vertex);
+                List<int> neighbours = graph.GetNeighbors(vertex);
+                localRejected.RemoveAll((x) => !neighbours.Contains(x));
+                localCandidates.RemoveAll((x) => !neighbours.Contains(x));
+                if (!localRejected.Any() && !localCandidates.Any())
+                {
+                    if (clique.Count > maxClique.Count)
+                    {
+                        maxClique = new List<int>(clique);
+                    }
+                }
+                else
+                {
+                    FindMaximumCliqueRec(ref graph, ref clique, ref localRejected, ref localCandidates);
+                }
+                clique.Remove(vertex);
+                rejected.Add(vertex);
+            }
+        }
+
+        public List<int> FindMaximumClique(Graph graph)
+        {
+            List<int> maximumClique = new List<int>();
+
+            List<int> clique;
+            List<int> rejected;
+            List<int> candidates;
+            PrepareSets(graph.VerticesCount, out clique, out rejected, out candidates);
+            FindMaximumCliqueRec(ref graph, ref clique, ref rejected, ref candidates);
+
+            return maxClique;
+        }
+    }
+}
---
+++ GraphDistance/GraphLibraryTests/ExactAlgorithmTest.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+using GraphLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GraphLibraryTests
+{
+    public class ExactAlgorithmTest
+    {
+        [Fact]
+        public void FindMaximumClique0()
+        {
+            var exactAlgorithms = new ExactAlgorithm();
+            var expected = new List<int> { 1, 2, 3, 4 };
+
+            var edges = new double[,]
+            {
+                { 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 1, 1, 0 },
+                { 0, 1, 0, 1, 1, 0 },
+                { 0, 1, 1, 0, 1, 0 },
+                { 0, 1, 1, 1, 0, 1 },
+                { 0, 0, 0, 0, 1, 0 },
+            };
+
+            var g = new Graph(edges);
+
+            var actual = exactAlgorithms.FindMaximumClique(g);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void FindMaximumClique1()
+        {
+            var exactAlgorithms = new ExactAlgorithm();
+            var expected = new List<int> { 2, 3, 4 };
+
+            var edges = new double[,]
+            {
+                { 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 0, 0, 0 },
+                { 0, 1, 0, 1, 1, 1 },
+                { 0, 0, 1, 0, 1, 0 },
+                { 0, 0, 1, 1, 0, 0 },
+                { 0, 0, 1, 0, 0, 0 },
+            };
+
+            var g = new Graph(edges);
+
+            var actual = exactAlgorithms.FindMaximumClique(g);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void FindMaximumClique2()
+        {
+            var exactAlgorithms = new ExactAlgorithm();
+            var expected = new List<int> { 0, 1, 2, 3, 4, 5 };
+
+            var edges = new double[,]
+            {
+                    { 0, 1, 1, 1, 1, 1, 0, 0, 0 },
+                    { 1, 0, 1, 1, 1, 1, 0, 0, 0 },
+                    { 1, 1, 0, 1, 1, 1, 0, 0, 0 },
+                    { 1, 1, 1, 0, 1, 1, 0, 0, 0 },
+                    { 1, 1, 1, 1, 0, 1, 0, 1, 1 },
+                    { 1, 1, 1, 1, 1, 0, 1, 0, 0 },
+                    { 0, 0, 0, 0, 0, 1, 0, 1, 0 },
+                    { 0, 0, 0, 0, 1, 0, 1, 0, 1 },
+                    { 0, 0, 0, 0, 1, 0, 0, 1, 0 }
+            };
+
+            var g = new Graph(edges);
+
+            var actual = exactAlgorithms.FindMaximumClique(g);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
