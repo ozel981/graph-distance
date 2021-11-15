feff4a85867bbcf0dc442e0cfd4dde736380bfb2
maxization <damian.opoka99@gmail.com>
2021-11-15 17:20:34 +0100

    add output folder

--- GraphDistance/publish.bat
+++ GraphDistance/publish.bat
@@ -1,4 +1,4 @@
 set gdpath=.\GraphDistance\bin\Release\net5.0\win10-x64
 if exist %gdpath% del %gdpath% /Q
-dotnet publish .\GraphDistance\GraphDistance.csproj -r win-x64 -c Release -p:PublishReadyToRunShowWarnings=true -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish .\GraphDistance\GraphDistance.csproj -r win-x64 -c Release -o publish -p:DebugType=None -p:DebugSymbols=false -p:PublishReadyToRunShowWarnings=true -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true
 PAUSE
